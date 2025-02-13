9f01199bcf10f0432c8e7fecbeb96eafde976133
Samrat Bhardwaj <samratbhardwaj@Samrats-MacBook-Pro.local>
2025-02-13 22:36:04 +0530

    Patterns first class

---
+++ Batch1/Patterns/Pattern1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Pattern1 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int total_number_of_lines = n;
+        int current_number_of_line = 1;
+
+        int stars = 1;
+
+        while(current_number_of_line <= total_number_of_lines){
+            // print stars
+            for(int i=1; i<=stars; i++){
+                System.out.print("*\t");
+            }
+
+            // print spaces
+
+            // prepare for next line
+            System.out.println();
+            stars++;
+            current_number_of_line++;
+        }
+    }
+}
