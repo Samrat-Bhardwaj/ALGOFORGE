f3fb5026de08e18f6b2b7122a35383fbd03836e6
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-03-11 23:42:17 +0530

    2D Arrays complete

--- Batch1/2D Arrays/Questions.java
+++ Batch1/2D Arrays/Questions.java
@@ -69,6 +69,82 @@ class Questions {
         }
     }
 
+    public void reverseEveryRow(int[][] matrix){
+        int n = matrix.length;
+
+        for(int row=0; row<n; row++){
+
+            int si = 0;
+            int ei = n-1; // arr[row].length - 1;
+
+            while(si < ei){
+                int temp = arr[row][si];
+
+                arr[row][si] = arr[row][ei];
+                arr[row][ei] = temp;
+
+                si++;
+                ei--;
+            }
+        }
+    }
+
+    // leetcode 48 ============================
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverseEveryRow(matrix);
+    }
+
+    // leetcode 74 ===================================
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int total_elements = n*m;
+        
+        int si = 0;
+        int ei = total_elements - 1;
+
+        while(si <= ei){
+            int mid = (si+ei)/2;
+
+            int i = mid/m;
+            int j = mid%m;
+
+            if(matrix[i][j] < target){
+                si = mid + 1;
+            } else if(matrix[i][j] > target){
+                ei = mid - 1;
+            } else {
+                // System.out.println("Element found at " + i + "," + j);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // leetcode 240 =======================================
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m-1;
+
+        while(row<n && col>=0){
+            if(matrix[row][col] < target){
+                row++;
+            } else if(matrix[row][col] > target){
+                col--;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args){
         // int[][] arr = {{11,22,4,45,110}, {33,9,7,5,89}, {66,77,6,55,99}, {13,1,43,88,44}};
         int[][] arr2 = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
