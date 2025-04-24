f4f7aabddf44eccf902b49059809d8ac8080f668
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-04-24 23:04:44 +0530

    Till Reverse of LinkedList

--- Batch1/LinkedList/Construction/Main.java
+++ Batch1/LinkedList/Construction/Main.java
@@ -78,6 +78,126 @@ class LinkedList {
         tail = temp;
         size--;
     }
+
+    public void removeFirst(){
+        if(head == null){ // size == 0, tail == null
+            System.out.println("There is nothing to remove");
+            return;
+        }
+        if(head.next == null){ // size = 1, head == tail
+            head = null;
+            tail = null;
+        } else {
+            Node headKaNext = head.next;
+            head = headKaNext;
+        }
+        size--;
+    }
+
+    public Node getNodeAt(int idx){
+        if(idx < 0 || idx >= size){
+            System.out.println("Index out of bounds");
+            return null;
+        }
+
+        Node temp = head;
+        for(int i=0; i<idx; i++){
+            Node tempKaNext = temp.next;
+            temp = tempKaNext;
+        }
+
+        return temp;
+    }
+
+    public void addNodeAt(int idx, int data){
+        if(idx < 0 || idx > size){
+            System.out.println("Index out of bounds");
+            return;
+        }
+
+        if(idx == 0){
+            addFirst(data);
+            return;
+        } else if(idx == size){
+            addLast(data);
+            return;
+        }
+
+        Node nn = new Node(data);
+        Node previousNode = getNodeAt(idx-1);
+        Node nextNode = previousNode.next;
+
+        previousNode.next = null; // breaking connection with rest of LL
+        previousNode.next = nn; // connecting previous node to new node
+
+        nn.next = nextNode; // connecting new node with rest of LL
+        this.size++;
+    }
+
+    public void removeNodeAt(int idx){
+        if(idx < 0 || idx >= size){
+            System.out.println("Index out of bounds");
+            return;
+        }
+
+        if(idx == 0){
+            removeFirst();
+            return;
+        }
+        if(idx == size - 1){
+            removeLast();
+            return;
+        }
+
+        Node prevNode = getNodeAt(idx-1);
+        Node toRemove = prevNode.next;
+        Node nextNode = toRemove.next;
+
+        prevNode.next = null; // breaking the connection with the node to be removed, removing connection from rest of LL
+        prevNode.next = nextNode; // connecting previous node with next of to be removed Node, thus connecting to rest of LL
+        this.size--;
+    }
+
+    public void reverseLLData(){
+        int i = 0;
+        int j = size-1;
+
+        while(i < j){
+            Node nodeAtI = getNodeAt(i);
+            Node nodeAtJ = getNodeAt(j);
+
+            int dataAtI = nodeAtI.data;
+            int dataAtJ = nodeAtJ.data;
+
+            nodeAtI.data = dataAtJ;
+            nodeAtJ.data = dataAtI;
+
+            i++;
+            j--;
+        }
+    }
+
+    public void reverseLLPointers(){
+        Node prev = null;
+        Node curr = head;
+        Node originalHead = head;
+
+        while(curr != null){
+            // saving next node
+            Node currKaNext = curr.next;
+
+            // changing connections
+            curr.next = null; // breaking connection with next
+            curr.next = prev; // making connection with previous node
+
+            // moving to next node
+            prev = curr;
+            curr = currKaNext;
+        }
+
+        head = prev;
+        tail = originalHead;
+    }
 }
 
 class Main {
@@ -86,22 +206,37 @@ class Main {
         
         for(int i=1; i<=4; i++){
             ll.addLast(i);
-            ll.display();
+            // ll.display();
         }
         // System.out.println(ll);
         ll.addFirst(55);
-        ll.display();
+        // ll.display();
         // System.out.println(ll);
 
         ll.addFirst(10);
         ll.display();
         // System.out.println(ll);
 
-        while(ll.size != 0){
-            ll.removeLast();
-            ll.display();
-        }
+        // while(ll.size != 0){
+        //     ll.removeFirst();
+        //     ll.display();
+        // }
 
-        ll.removeLast();
+        // ll.removeFirst();
+        // Node n = ll.getNodeAt(4);
+        // System.out.println(n.data);
+        // System.out.println(n.next);
+
+        // ll.removeNodeAt(2);
+        // ll.display();
+
+        // ll.removeNodeAt(0);
+        // ll.display();
+
+        ll.reverseLLPointers();
+        ll.display();
+
+        ll.reverseLLPointers();
+        ll.display();
     }
 }
