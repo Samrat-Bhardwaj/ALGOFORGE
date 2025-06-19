cafd1459e44d45987793319de1d1a1a1be9b7d31
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-06-19 21:11:52 +0530

    Binary Tree construction

--- Batch1/Trees/Binary Tree/Introduction/Main.java
+++ Batch1/Trees/Binary Tree/Introduction/Main.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 class Node {
     int data;
     Node left;
@@ -20,9 +21,46 @@ class Pair {
 
 
 class Main {
-    public static void main(String[] args){
-        Integer[] arr = {10,20,11,null,15,null,null,12,14,null,null,null,30,null,13,16,null,null,17,null,null};
+    public static int size(Node root){
 
+    }
+
+    public static int sum(Node root){
+        
+    }
+
+    public static int maxOfBinaryTree(Node root){
+        
+    }
+
+    public static int height(Node root){
+        
+    }
+    
+    public static void display(Node root){
+        if(root == null){
+            return;
+        }
+
+        String currNodeStucture = (root.left == null ? "." : root.left.data) + " <- " + root.data + " -> " + (root.right == null ? "." : root.right.data);
+        // String currNodeStucture = "";
+        // if(root.left != null){
+        //     currNodeStucture += root.left.data + " <- ";
+        // }
+
+        // currNodeStucture += root.data;
+
+        // if(root.right != null){
+        //     currNodeStucture += " -> " + root.right.data;
+        // }
+
+        System.out.println(currNodeStucture);
+
+        display(root.left);
+        display(root.right);
+    }
+
+    public static Node constructTree(Integer[] arr){
         Node root = null;
         Stack<Pair> st = new Stack<>();
 
@@ -43,8 +81,17 @@ class Main {
                 st.peek().node.right = newNode;
                 st.pop();
             }
-
-            st.push(new Pair(newNode, 1));
+            if(newNode != null){
+                st.push(new Pair(newNode, 1));
+            }
         }
+
+        return root;
+    }
+
+    public static void main(String[] args){
+        Integer[] arr = {10,20,11,null,15,null,null,12,14,null,null,null,30,null,13,16,null,null,17,null,null};
+        Node root = constructTree(arr);
+        display(root);
     }
 }
