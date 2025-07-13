3f7dce0eadd1ba2f92684098c8e0617a7f226f56
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-07-13 22:22:25 +0530

    Topological sort cycle problems

--- Batch1/Graph/DFS/Questions.java
+++ Batch1/Graph/DFS/Questions.java
@@ -231,4 +231,131 @@ class Questions {
 
         return totalPerimeter;
     }
+
+    // TOPOLOGICAL ORDER ==================================================================
+    
+    // Is course scheduling possible (Leetcode 207) =============================
+    public boolean checkCycle(int src, ArrayList<Integer>[] graph, int[] vis){
+        vis[src] = 1;
+
+        for(int nbr: graph[src]){
+            if(vis[nbr] == 1){
+                return true;
+            } else if(vis[nbr] == 0){
+                if(checkCycle(nbr,graph, vis)) return true;
+            }
+        }
+
+        vis[src] = 2;
+        return false;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+
+        for(int i=0; i<numCourses; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int[] edge: prerequisites){
+            int u = edge[1];
+            int v = edge[0];
+
+            graph[u].add(v);
+        }
+
+        int[] vis = new int[numCourses];
+        for(int i=0; i<numCourses; i++){
+            if(vis[i] == 0){
+                if(checkCycle(i,graph,vis)) return false; // no possible topological order
+            }
+        }
+
+        return true;
+    }
+
+    // Find course scheduling order(Leetcode 210) =========================================
+    public boolean topo_dfs(int src, ArrayList<Integer>[] graph, int[] vis, ArrayList<Integer> order){
+        vis[src] = 1;
+
+        for(int nbr: graph[src]){
+            if(vis[nbr] == 1){
+                return true;
+            } else if(vis[nbr] == 0){
+                if(topo_dfs(nbr,graph, vis,order)) return true;
+            }
+        }
+
+        vis[src] = 2;
+        order.add(src);
+        return false;
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        ArrayList<Integer> order = new ArrayList<>();
+
+        for(int i=0; i<numCourses; i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int[] edge: prerequisites){
+            int u = edge[1];
+            int v = edge[0];
+
+            graph[u].add(v);
+        }
+
+        int[] vis = new int[numCourses];
+
+        for(int i=0; i<numCourses; i++){
+            if(vis[i] == 0){
+                if(topo_dfs(i,graph,vis, order)){ // no possible topological order
+                    return new int[]{};
+                }
+            }
+        }
+
+        Collections.reverse(order);
+        int[] ansArray = order.stream().mapToInt(Integer::intValue).toArray(); // converting arraylist to array
+
+        return ansArray;
+    }
+
+    // Find eventual safe states (Leetcode 802) =============================================
+    public boolean dfs(int src, int[][] graph, int[] vis){
+        vis[src] = 1;
+
+        for(int nbr: graph[src]){
+            if(vis[nbr] == 1){
+                return true;
+            } else if(vis[nbr] == 0) {
+                if(dfs(nbr,graph,vis) == true) return true;
+            }
+        }
+
+        vis[src] = 2;
+        return false;
+    }
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+
+        int[] vis = new int[n];
+
+        for(int i=0; i<n; i++){
+            if(vis[i] == 0){
+                dfs(i,graph,vis);
+            }
+        }
+
+        List<Integer> safeStates = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(vis[i] == 2){
+                safeStates.add(i);
+            }
+        }
+        
+        return safeStates;
+    }
 }
--- Batch1/Graph/Introduction/Directed Graph/TopologicalSort.java
+++ Batch1/Graph/Introduction/Directed Graph/TopologicalSort.java
@@ -17,6 +17,7 @@ class Edge {
 }
 
 class TopologicalSort {
+    // Topological Sort for DAG (Directed Acyclic Graph) =======================================
     public static void topo_dfs(int src,boolean[] vis,ArrayList<Edge>[] graph, int N, ArrayList<Integer> topologicalOrder){
         vis[src] = true;
 
@@ -42,8 +43,44 @@ class TopologicalSort {
 
         Collections.reverse(topologicalOrder);
         return topologicalOrder;
+    }   
+
+    // Topological Sort for Directed Graph =========================
+    public static boolean topo_dfs_generic(int src,int[] vis,ArrayList<Edge>[] graph, int N, ArrayList<Integer> topologicalOrder){
+        vis[src] = 1;
+
+        for(Edge e: graph[src]){
+            int nbr = e.v;
+            if(vis[nbr] == 1){ // nbr is part of current path -> cycle presemt
+                return false;
+            } else if(vis[nbr] == 2){ // nbr is already visited but not part of current path
+                continue;
+            } else {
+                if(topo_dfs_generic(nbr,vis,graph,N,topologicalOrder) == false) return false;
+            }
+        }
+
+        vis[src] = 2; // all paths exploration done
+        topologicalOrder.add(src);
+        return true;
     }
 
+    public static ArrayList<Integer> topologicalSort_generic(ArrayList<Edge>[] graph, int N){
+        ArrayList<Integer> topologicalOrder = new ArrayList<>();
+        int[] vis = new boolean[N];
+
+        for(int i=0; i<N; i++){
+            if(vis[i] == 0){
+                if(topo_dfs(i,vis,graph,N,topologicalOrder) == false){
+                    System.out.println("No Possible Solution"); // there is cycle, so no possible solution
+                    return new ArrayList<>();
+                }
+            }
+        }
+
+        Collections.reverse(topologicalOrder);
+        return topologicalOrder;
+    }
     public static void displayGraph(ArrayList<Edge>[] graph){
         for(int i=0; i<graph.length; i++){
             System.out.print("Edges on vertex " + i + " -> ");
