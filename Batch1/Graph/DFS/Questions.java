c5a2178944fc31d97e96331b258c92c84843cb3a
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-07-19 18:54:04 +0530

    DSU Implementation

--- Batch1/Graph/DFS/Questions.java
+++ Batch1/Graph/DFS/Questions.java
@@ -358,4 +358,64 @@ class Questions {
         
         return safeStates;
     }
+
+    // Kosaraju, storngly connected comp (https://www.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1)
+    public static void dfs_fill_stack(int src,boolean[] vis,ArrayList<ArrayList<Integer>> graph,Stack<Integer> st){
+        vis[src] = true;
+
+        for(int nbr: graph.get(src)){
+            if(!vis[nbr]){
+                dfs_fill_stack(nbr,vis,graph,st);
+            }
+        }
+
+        st.push(src);
+    }
+    // just traverse through the component
+    public static void dfs(int src, boolean[] vis,ArrayList<Integer>[] reversedGraph){
+        vis[src] = true;
+
+        for(int nbr: reversedGraph[src]){
+            if(!vis[nbr]){
+                dfs(nbr,vis,reversedGraph);
+            }
+        }
+    }
+
+    public int kosaraju(ArrayList<ArrayList<Integer>> adj) {
+        Stack<Integer> st = new Stack<>();
+        int N = adj.size();
+        boolean[] vis = new boolean[N];
+
+        for(int i=0; i<N; i++){
+            if(!vis[i]){
+                dfs_fill_stack(i,vis,adj,st);
+            }
+        }
+
+        ArrayList<Integer>[] reversedGraph = new ArrayList[N];
+        for(int i=0; i<N; i++){
+            reversedGraph[i] = new ArrayList<>();
+        }
+
+        for(int u=0; u<N; u++){
+            for(int i=0; i<adj.get(u).size(); i++){
+                int v = adj.get(u).get(i);
+                reversedGraph[v].add(u);
+            }
+        }
+
+        int num_scc = 0;
+        vis = new boolean[N];
+        while(st.size() > 0){
+            int vtx = st.pop();
+
+            if(!vis[vtx]){
+                dfs(vtx,vis,reversedGraph);
+                num_scc++;
+            }
+        }
+
+        return num_scc;
+    }
 }
