ffcdfef755ce6b648b4b8f31166ebec89e8d4194
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-07-24 23:09:04 +0530

    DSU and BFS Problems

--- Batch1/Graph/Union Find/questions.cpp
+++ Batch1/Graph/Union Find/questions.cpp
@@ -46,6 +46,114 @@ public:
     }
 };
 
+// Similar string groups (Leetcode 839) ===========================
+class Solution {
+public:
+    bool isSimilar(string &a, string &b){
+        int diff_char = 0;
+
+        for(int i=0; i<a.size() && diff_char < 3; i++){
+            if(a[i] != b[i]){
+                diff_char++;
+            }
+        }
+
+        return diff_char <= 2;
+    }
+
+    unordered_map<string,string> par;
+    string findPar(string& u){
+        if(par[u] == u) return u;
+
+        return par[u] = findPar(par[u]);
+    }
+    
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+
+        for(int i=0; i<strs.size(); i++){
+            par[strs[i]] = strs[i];
+        }
+        int total_comps = par.size(); // so that duplicate elements are not considered twice 
+
+        for(int i=0; i<n; i++){
+            string p1 = findPar(strs[i]);
+
+            for(int j=i+1; j<n; j++){
+                if(isSimilar(strs[i],strs[j])){
+                    string p2 = findPar(string[j]);
+
+                    if(p1 != p2){
+                        par[p2] = p1;
+                        total_comps--;
+                    }
+                }
+            }
+        }
+
+        return total_comps;
+    }
+};
+
+// leetcode 990 (Satisfiability of Equality Equations) ==============================
+class Solution {
+public:
+    vector<int> par;
+    vector<int> size;
+    int findPar(int u){
+        if(par[u] == u) return u;
+
+        return par[u] = findPar(par[u]);
+    }
+
+    void merge(int p1, int p2){
+        if(size[p1] < size[p2]){
+            par[p1] = p2;
+            size[p2] += size[p1];
+        } else {
+            par[p2] = p1;
+            size[p1] += size[p2];
+        }
+    }
+
+    bool equationsPossible(vector<string>& equations) {
+        par.resize(26,0);
+        size.resize(26,1);
+
+        for(int i=0; i<26; i++){
+            par[i] = i;
+        }
+
+        for(string &eq: equations){
+            if(eq[1] == '!') continue;
+
+            int u = eq[0] - 'a';
+            int v = eq[3] - 'a';
+
+            int p1 = findPar(u);
+            int p2 = findPar(v);
+
+            if(p1 != p2){ // combining equations with equal sign
+                merge(p1,p2);
+            }
+        }
+
+        for(string &eq: equations){
+            if(eq[1] == '=') continue;
+
+            int u = eq[0] - 'a';
+            int v = eq[3] - 'a';
+
+            int p1 = findPar(u);
+            int p2 = findPar(v);
+
+            if(p1 == p2) return false; // two variables with ! sign should not have same parent
+        }
+
+        return true;
+    }
+};
+
 
 
 
