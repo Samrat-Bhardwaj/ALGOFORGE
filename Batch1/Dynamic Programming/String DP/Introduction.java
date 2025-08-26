f9cb55e720a353e4675cd1bdd1bd8553f11b6e19
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-08-26 23:16:30 +0530

    String dp Problems

--- Batch1/Dynamic Programming/String DP/Introduction.java
+++ Batch1/Dynamic Programming/String DP/Introduction.java
@@ -145,6 +145,176 @@ class Introduction {
         return lps_tab_pretty(s);
     }
 
+    //Homework =  Generate longest pallindromic subsequence string =====================
+
+    public int numDistinct_rec(int n, int m,String s, String t, int[][] dp){
+        if(m == 0){
+            return dp[n][m] = 1;
+        }
+
+        if(n < m){
+            return dp[n][m] = 0;
+        }
+
+        if(dp[n][m] != -1) return dp[n][m];
+
+        int count = 0;
+        if(s.charAt(n-1) == t.charAt(m-1)){ // you can include or not include curr char
+            count = numDistinct_rec(n-1,m-1,s,t,dp) + numDistinct_rec(n-1,m,s,t,dp);
+        } else { // can't include curr char
+            count = numDistinct_rec(n-1,m,s,t,dp);
+        }
+
+        return dp[n][m] = count;
+    }
+
+    public int numDistinct_tab(int N, int M,String s, String t, int[][] dp){
+        for(int m=0; m<=M; m++){
+            for(int n=0; n<=N; n++){
+                if(m == 0){
+                    dp[n][m] = 1;
+                } else if(n < m){
+                    dp[n][m] = 0;
+                } else {
+                    if(s.charAt(n-1) == t.charAt(m-1)){
+                        dp[n][m] = dp[n-1][m-1] + dp[n-1][m];
+                    } else {
+                        dp[n][m] = dp[n-1][m];
+                    }
+                }
+            }
+        }
+
+        return dp[N][M];
+    }
+
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length(); 
+
+        int[][] dp = new int[n+1][m+1];
+        // for(int[] d: dp) Arrays.fill(d, -1);
+
+        // return numDistinct_rec(n,m,s,t,dp);
+        return numDistinct_tab(n,m,s,t,dp);
+    }
+
+    // Longest common subsequence (LCS) (Leetcode 1143) =======================================
+    public int lcs_rec(int n, int m, String s1, String s2, int[][] dp){
+        if(n == 0 || m == 0){
+            return dp[n][m] = 0;
+        }
+
+        if(dp[n][m] != -1) return dp[n][m];
+
+        int ans = 0;
+        if(s1.charAt(n-1) == s2.charAt(m-1)){
+            ans = lcs_rec(n-1,m-1,s1,s2,dp) + 1;
+        } else {
+            ans = Math.max(lcs_rec(n-1,m,s1,s2,dp), lcs_rec(n,m-1,s1,s2,dp));
+        }
+
+        return dp[n][m] = ans;
+    }
+
+    public int lcs_tab(int n, int m, String s1, String s2, int[][] dp){
+        
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                } else {
+                    if(s1.charAt(i-1) == s2.charAt(j-1)){
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    public int lcs_tabWithString(int n, int m, String s1, String s2, int[][] dp){
+        String[][] sdp = new int[n+1][m+1];
+
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                    sdp[i][j] = ""; 
+                } else {
+                    if(s1.charAt(i-1) == s2.charAt(j-1)){
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                        sdp[i][j] = sdp[i-1][j-1] + s1.charAt(i-1);
+                    } else if(dp[i-1][j] < dp[i][j-1]){
+                        dp[i][j] = dp[i][j-1];
+                        sdp[i][j] = sdp[i][j-1];
+                    } else {
+                        dp[i][j] = dp[i-1][j];
+                        sdp[i][j] = sdp[i-1][j];
+                    }
+                }
+            }
+        }
+
+        System.out.println(sdp[n][m]);
+        return dp[n][m];
+    }
+
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n+1][m+1];
+        // for(int[] d: dp) Arrays.fill(d, -1);
+
+        // return lcs_rec(n,m,s1,s2,dp);
+        return lcs_tabWithString(n,m,s1,s2,dp);
+    }
+
+    // leetcode 1035 (Max uncrossed lines) ================================
+    public int lcs_tab(int n, int m, int[] nums1, int[] nums2){
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 0;
+                } else {
+                    if(nums1[i-1] == nums2[j-1]){
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    } else {
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        return lcs_tab(n,m,nums1,nums2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
