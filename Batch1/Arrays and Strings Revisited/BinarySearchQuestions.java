4963912d289055180f2755c2066e9d29b8007202
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-09-27 12:31:22 +0530

    Binary Search on Answers

--- Batch1/Arrays and Strings Revisited/BinarySearchQuestions.java
+++ Batch1/Arrays and Strings Revisited/BinarySearchQuestions.java
@@ -103,7 +103,7 @@ class BinarySearchQuestions {
         return ans.subList(si,ei+1);
     }
 
-    // Aggressive cows ==================================================
+    // Aggressive cows (maximize the minimum distance) ==================================================
     public boolean isPossibleToPlaceCows(int[] stalls, int minDis, int k){
         int lastPlacedCowPos = stalls[0];
         k--;
@@ -172,5 +172,245 @@ class BinarySearchQuestions {
         }
 
         return minSpeed;
+    }
+
+    // Leetcode 2226 (Maximum candies allocated) ================================
+    public boolean isPossibleToGiveXCandies(int[] candies, long k, int X){
+        for(int c : candies){
+            int childrenCovered = c/X;
+
+            k -= childrenCovered;
+        }
+
+        return k <= 0;
+    }
+
+    public int maximumCandies(int[] candies, long k) {
+        int min = 1; // si, low, left
+        int max = (int)(1e7); // ei, high, right
+
+        int maxPossibleCandies = 0;
+        while(min <= max){
+            int mid = (min + max)/2;
+
+            if(isPossibleToGiveXCandies(candies,k,mid)){ // O(n)
+                maxPossibleCandies = mid;
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+
+        return maxPossibleCandies;
+    }
+
+    // Leetcode 1482 (Minimum Number of Days to Make m Bouquets)
+    public boolean isPossibleToMakeBounquetsOnDDay(int[] bloomDay, int m, int k, int day){
+        int count = 0;
+
+        for(int i=0; i<bloomDay.length; i++){
+            if(bloomDay[i] <= day){
+                count++;
+                if(count == k){ // we found k adjacent flowers
+                    m--;
+                    count = 0;
+                }
+            } else {
+                count = 0;
+            }
+
+            if(m == 0) return true;
+        }
+
+        return false;
+    }
+
+    public int minDays(int[] bloomDay, int m, int k) {
+        int left = 1;
+        int right = (int)1e9;
+
+        int minPossibleDay = -1;
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(isPossibleToMakeBounquetsOnDDay(bloomDay, m, k, mid)){
+                minPossibleDay = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return minPossibleDay;
+    }
+
+    // Leetcode 1011 || Capacity To Ship Packages Within D Days
+    public boolean isPossibleToShipWithCapacity(int[] weights, int days, int maxCapacity){
+        int currCapacity = 0;
+
+        for(int i=0; i<weights.length; i++){
+            currCapacity += weights[i];
+
+            if(currCapacity > maxCapacity){
+                days--;
+                currCapacity = weights[i];
+            }
+        }
+
+        // if(currCapacity > 0) days--;
+        // return days >= 0;
+
+        return days > 0;
+    }
+
+    public int shipWithinDays(int[] weights, int days) {
+        int maxWeight = 0;
+        int sumOfWeights = 0;
+
+        for(int w: weights){
+            maxWeight = Math.max(maxWeight, w);
+            sumOfWeights += w;
+        }    
+
+        int left = maxWeight; // can start with left = 1 and right = 1e8
+        int right = sumOfWeights;
+
+        int minCapacity = 0;
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(isPossibleToShipWithCapacity(weights, days, mid)){
+                minCapacity = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return minCapacity;
+    }
+
+    // Allocate minimum pages (https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1)
+    class Solution {
+        // is Possible to divide all books into k or less students such 
+        // that no student will get more than maxPages pages
+        public boolean isPossible(int[] arr, int maxPages, int k){
+            int currentSumOfPages = 0;
+
+            for(int i=0; i<arr.length; i++){
+                currentSumOfPages += arr[i];
+
+                if(currentSumOfPages > maxPages){
+                    k--;
+                    currentSumOfPages = arr[i];
+                }
+            }
+
+            // need one student for currentSumOfPages 
+            return k > 0;
+        }
+        
+        public int findPages(int[] arr, int k) {
+            if(arr.length < k){
+                return -1;
+            }
+            int maxPage = 0;
+            int sumOfPages = 0;
+            for(int pages: arr){
+                maxPage = Math.max(pages, maxPage);
+                sumOfPages += pages;
+            } 
+            // code here
+            int left = maxPage; // (ans for k == arr.length)
+            int right = sumOfPages; // (ans for k == 1)
+
+            int minMaxPages = 0; // minimizing the maximum page
+            while(left <= right){
+                int mid = (left + right)/2;
+
+                if(isPossible(arr, mid, k)){
+                    minMaxPages = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return minMaxPages;
+        }
+    }
+
+    // Leetcode 410 (copy paste of above 2) (Split Array Largest Sum)
+    public boolean isPossible(int[] arr, int maxSum, int k){
+        int currentSum = 0;
+
+        for(int i=0; i<arr.length; i++){
+            currentSum += arr[i];
+
+            if(currentSum > maxSum){
+                k--;
+                currentSum = arr[i];
+            }
+        }
+
+        // need one subarray for csum 
+        return k > 0;
+    }
+
+    public int splitArray(int[] nums, int k) {
+        int maxSum = 0;
+        int sum = 0;
+        for(int num: nums){
+            maxSum = Math.max(num, maxSum);
+            sum += num;
+        } 
+        // code here
+        int left = maxSum;
+        int right = sum; 
+
+        int minMaxSum = 0; 
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(isPossible(nums, mid, k)){
+                minMaxSum = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return minMaxSum;
+    }
+
+    // Minimize max distance to gas station (https://www.geeksforgeeks.org/problems/minimize-max-distance-to-gas-station/1) ==========================
+    public boolean isPossible(int[] stations, int k, double mid){
+        for(int i=1; i<stations.length; i++){
+            int disBetweenLast2 = stations[i] - stations[i-1];
+            
+            int newStationsRequired = (int)(disBetweenLast2/mid);
+            k -= newStationsRequired;
+            
+        }   
+        
+        return k >= 0;
+    }
+    
+    public double minMaxDist(int[] stations, int K) {
+        double left = 0.0, right = 1e9, mid = 0.0;
+        double ans = 0.0;
+        while(left <= right){
+            mid = (left + right)/2.0;
+            
+            if(isPossible(stations,K, mid)){
+                ans = mid;
+                right = mid - 1e-6; // (mid - 0.000001) // we can also move by 0.001 
+                // Why? Because we need answer upto 2 decimal points, so we should move by 3 decimal points
+            } else {
+                left = mid + 1e-6; // (mid + 0.000001)
+            }
+        }
+        
+        return mid;
     }
 }
