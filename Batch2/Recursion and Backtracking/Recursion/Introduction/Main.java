92998df9fa69608468eda193b7d56dd3cb4dea92
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-10-03 22:43:13 +0530

    Tower Of Hanoi

--- Batch2/Recursion and Backtracking/Recursion/Introduction/Main.java
+++ Batch2/Recursion and Backtracking/Recursion/Introduction/Main.java
@@ -68,8 +68,42 @@ class Main {
         return ans;
     }
 
+    public static void printZigZag(int n){
+        if(n == 0){
+            return;
+        }
+        // pre-area (before any recursive calls are made)
+        System.out.println("pre-area "+n);
+
+        printZigZag(n-1);
+
+        // in-area (between any 2 recursive calls)
+        System.out.println("in-area "+n);
+
+        printZigZag(n-1);
+
+        // post-area (after all the recursive calls are made)
+        System.out.println("post-area"+n);
+    }
+
+    public static void towerOfHanoi(int n, int A, int C, int B){
+        if(n == 0){
+            return;
+        }
+
+        towerOfHanoi(n-1, A, B, C);
+
+        System.out.println("Moving " + n + " disc from " + A + " to " + C);
+
+        towerOfHanoi(n-1, B, C, A);
+    }
+
     public static void main(String[] args){
-        int x = 2, y = 6;
-        System.out.println(power_log(x,y));
+        // int x = 2, y = 6;
+        // System.out.println(power_log(x,y));
+
+        // printZigZag(3);
+
+        towerOfHanoi(3,10,30,20);
     }
 }
