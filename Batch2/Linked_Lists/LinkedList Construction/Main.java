bebec8b6bffdddb9ab8bcb148525dde25fde3dbc
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-11-18 22:55:27 +0530

    LinkedList Introduction

--- Batch2/Linked_Lists/LinkedList Construction/Main.java
+++ Batch2/Linked_Lists/LinkedList Construction/Main.java
@@ -21,20 +21,66 @@ class LinkedList {
     }
 
     public void addLast(int val){
+        Node newNode = new Node(val);
 
-    }
-
-    public String toString(){
-        String list = "";
-
-        Node temp = head;
-        while(temp != null){
-            list += temp.data + ", ";
-            temp = temp.next;
+        if(head == null){ // size = 0;
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
         }
 
-        return list;
+        this.size++;
     }
+
+    public void addFirst(int val){
+        Node newNode = new Node(val);
+
+        if(this.head == null){ // size = 0;
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+
+        this.size++;
+    }
+
+    public void removeLast(){
+        if(head == null){ // size == 0;
+            System.out.println("Nothing to remove, LinkedList is empty!!");
+            return;
+        } else if(head == tail){ // size = 1
+            head = null;
+            tail = null;
+        } else {
+            // reach second last node
+            Node temp = head;
+            while(temp.next != tail){
+                Node tempKaNext = temp.next;
+                temp = tempKaNext;
+            }
+
+            temp.next = null; // removing last node
+            tail = temp; // shifting tail
+        }
+
+        this.size--;
+    }
+
+    public void display(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data + ", ");
+
+            Node tempKaNext = temp.next;
+            temp = tempKaNext;
+        }
+        System.out.println();
+    }    
 }
 
 class Main {
@@ -42,8 +88,12 @@ class Main {
         LinkedList ll = new LinkedList();
 
         ll.addLast(23);
+        ll.addLast(43);
         ll.addLast(13);
+        ll.addFirst(10);
+        ll.addFirst(5);
+        ll.removeLast();
 
-        System.out.println(ll);
+        ll.display();
     }
 }
