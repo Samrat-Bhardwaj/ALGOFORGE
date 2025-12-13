d638c0c7e921915cedd05a3bd9dfab3d625ab282
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-12-13 22:07:42 +0530

    Infix postfix evaluation

--- Batch2/Stack and Queue/Stack/Questions/Questions.java
+++ Batch2/Stack and Queue/Stack/Questions/Questions.java
@@ -1,4 +1,4 @@
-import java.util.Stack;
+import java.util.*;
 class Questions {
     // check for duplicate bracket 
     public static boolean isDuplicate(String exp){
@@ -155,7 +155,7 @@ class Questions {
     }
 
     // leetcode 84 (Largest area histogram)
-    public int largestRectangleArea(int[] heights) {
+    public int largestRectangleArea2(int[] heights) {
         int n = heights.length;
         Stack<Integer> st = new Stack<>();
         st.push(-1);
@@ -189,6 +189,238 @@ class Questions {
         return maxArea;
     }
 
+    // ================================== QUESTIONS ON POSTFIX INFIX PREFIX EXPRESSIONS ==============================
+
+    // Infix evaluation =================
+    public static int precedence(char ch){
+        if(ch == '+' || ch == '-'){
+            return 1;
+        } else if(ch == '/' || ch == '*'){ 
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int evaluate(int v1, int v2, char op){
+        if(op == '+'){
+            return v1 + v2;
+        } else if(op == '-'){
+            return v1 - v2;
+        } else if(op == '*'){
+            return v1 * v2;
+        } else {
+            return v1 / v2;
+        }
+    }
+
+    public static int evaluateInfixExp(String exp){
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        for(int i=0; i<exp.length(); i++){
+            char ch = exp.charAt(i); 
+
+            if(ch >= '0' && ch <= '9'){
+                operands.push(ch - '0');
+            } else if(ch == '('){
+                operators.push(ch);
+            } else if(ch == '+' || ch == '-' || ch =='/' || ch == '*'){
+
+                while(operators.size() > 0 && precedence(operators.peek()) >= precedence(ch)){
+                    char op = operators.pop();
+                    int v2 = operands.pop();
+                    int v1 = operands.pop();
+
+                    int res = evaluate(v1,v2,op);
+
+                    operands.push(res);
+                }
+
+                operators.push(ch);
+            } else if(ch == ')'){
+                
+                while(operators.peek() != '('){
+                    char op = operators.pop();
+                    int v2 = operands.pop();
+                    int v1 = operands.pop();
+
+                    int res = evaluate(v1,v2,op);
+
+                    operands.push(res);
+                }
+
+                operators.pop(); // popping '('
+            } 
+        }
+
+        while(operators.size() > 0){
+            char op = operators.pop();
+            int v2 = operands.pop();
+            int v1 = operands.pop();
+
+            int res = evaluate(v1,v2,op);
+
+            operands.push(res);
+        }
+
+        return operands.peek();
+    }
+
+    // Infix to postfix and prefix expressions ============================
+
+    public static void convertInfixToPrePost(String exp){
+        Stack<Character> operators = new Stack<>();
+        Stack<String> prefix = new Stack<>();
+        Stack<String> postfix = new Stack<>();
+
+        for(int i=0; i<exp.length(); i++){
+            char ch = exp.charAt(i);
+
+            if(ch >= '0' && ch <= '9'){
+                prefix.push(ch + "");
+                postfix.push(ch + "");
+            } else if(ch == '('){
+                operators.push(ch);
+            } else if(ch == '+' || ch == '-' || ch == '/' || ch == '*'){
+
+                while(operators.size() > 0 && precedence(operators.peek()) >= precedence(ch)){
+
+                    char op = operators.pop();
+
+                    // create prefix
+                    String v2 = prefix.pop();
+                    String v1 = prefix.pop();
+
+                    String preRes = op + v1 + v2;
+                    prefix.push(preRes);
+
+                    // create postfix
+                    String v2 = postfix.pop();
+                    String v1 = postfix.pop();
+
+                    String postRes = v1 + v2 + op;
+                    postfix.push(postRes);
+                }
+
+                operators.push(ch);
+            } else if(ch == ')'){
+
+                while(operators.peek() != '('){
+                    char op = operators.pop();
+
+                    // create prefix
+                    String v2 = prefix.pop();
+                    String v1 = prefix.pop();
+
+                    String preRes = op + v1 + v2;
+                    prefix.push(preRes);
+
+                    // create postfix
+                    String v2 = postfix.pop();
+                    String v1 = postfix.pop();
+
+                    String postRes = v1 + v2 + op;
+                    postfix.push(postRes);
+                }
+            }
+        }
+
+        while(operators.size() > 0){
+            char op = operators.pop();
+
+            // create prefix
+            String v2 = prefix.pop();
+            String v1 = prefix.pop();
+
+            String preRes = op + v1 + v2;
+            prefix.push(preRes);
+
+            // create postfix
+            String v2 = postfix.pop();
+            String v1 = postfix.pop();
+
+            String postRes = v1 + v2 + op;
+            postfix.push(postRes);
+        }
+
+        System.out.println("Prefix expression is: " + prefix.peek());
+        System.out.println("Postfix expression is: " + postfix.peek());
+    }
+
+    // to submit on GFG (^ dont have left to right precedence but right to left)
+    class Solution {
+    public int precedence(char ch){
+        if(ch == '^'){
+            return 3;
+        }else if(ch == '+' || ch == '-'){
+            return 1;
+        } else if(ch == '/' || ch == '*'){ 
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+    
+    public String infixToPrefix(String exp) {
+        // code here
+        Stack<Character> operators = new Stack<>();
+        Stack<String> prefix = new Stack<>();
+
+        for(int i=0; i<exp.length(); i++){
+            char ch = exp.charAt(i);
+
+            if(ch == '('){
+                operators.push(ch);
+            } else if(ch == '+' || ch == '-' || ch == '/' || ch == '*' || ch =='^'){
+
+                while(operators.size() > 0 && (precedence(operators.peek()) > precedence(ch) || (precedence(operators.peek()) == precedence(ch) && ch !='^'))){
+
+                    char op = operators.pop();
+
+                    // create prefix
+                    String v2 = prefix.pop();
+                    String v1 = prefix.pop();
+
+                    String preRes = op + v1 + v2;
+                    prefix.push(preRes);
+                }
+
+                operators.push(ch);
+            } else if(ch == ')'){
+
+                while(operators.peek() != '('){
+                    char op = operators.pop();
+
+                    // create prefix
+                    String v2 = prefix.pop();
+                    String v1 = prefix.pop();
+
+                    String preRes = op + v1 + v2;
+                    prefix.push(preRes);
+                }
+                
+                operators.pop();
+            } else {
+                prefix.push(ch + "");
+            }
+        }
+
+        while(operators.size() > 0){
+            char op = operators.pop();
+
+            // create prefix
+            String v2 = prefix.pop();
+            String v1 = prefix.pop();
+
+            String preRes = op + v1 + v2;
+            prefix.push(preRes);
+        }
+
+        return prefix.peek();
+    }
+}
+
 
 
 
@@ -216,10 +448,13 @@ class Questions {
 
 
     public static void main(String[] args){
-        String exp1 = "((a+b)+(c+d))";
-        String exp2 = "((a+b)+((c+d)))";
+        // String exp1 = "((a+b)+(c+d))";
+        // String exp2 = "((a+b)+((c+d)))";
 
-        System.out.println(isDuplicate(exp1));
-        System.out.println(isDuplicate(exp2));
+        // System.out.println(isDuplicate(exp1));
+        // System.out.println(isDuplicate(exp2));
+
+        String infix =  "2 + 3 * 5 - 8 + ( 4 / 3 + 5 ) * 9";
+        System.out.println(evaluateInfixExp(infix));
     }
 }
