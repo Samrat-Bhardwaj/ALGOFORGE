286008c02896ed652b7b7b3f2b107e2b8da70f1b
Samrat Bhardwaj <bsamrat47@gmail.com>
2025-12-18 20:19:37 +0530

    Stack Using Queue Question code

---
+++ Batch2/Stack and Queue/Construction/StackUsingQueuePushEfficient.java
@@ -0,0 +1,20 @@
+class MyStack {
+    private LinkedList<Integer> que;
+
+    public MyStack(){}
+
+    // O(1)
+    public void push(int val){}
+
+    public int pop(){}
+
+    public int peek(){}
+}
+
+class StackUsingQueuePushEfficient {
+    public static void main(String[] args){
+        MyStack st = new MyStack();
+
+
+    }
+}
