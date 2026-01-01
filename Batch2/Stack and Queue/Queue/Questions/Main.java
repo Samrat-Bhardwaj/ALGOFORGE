23c70b42650f5d5d48356dec88e7b54647ddf8c7
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-01-01 22:21:29 +0530

    Queue end

--- Batch2/Stack and Queue/Queue/Questions/Main.java
+++ Batch2/Stack and Queue/Queue/Questions/Main.java
@@ -110,6 +110,45 @@ class Main {
         return ans;
     }
 
+    // leetcode 862 ===========================================================
+    public int shortestSubarray(int[] nums, int k) {
+        int n = nums.length;
+
+        long[] pre = new long[n];
+        pre[0] = nums[0];
+
+        for(int i=1; i<n; i++){
+            pre[i] = pre[i-1] + nums[i];
+        }
+
+        LinkedList<Integer> deque = new LinkedList<>();
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++){
+            if(pre[i] >= k){
+                ans = Math.min(i+1, ans);
+            }
+
+            // this is to shorten the array
+            while(deque.size()>0 && pre[i] - pre[deque.getFirst()] >= k){
+                ans = Math.min(ans, i - deque.getFirst());
+                deque.removeFirst();
+            }
+
+            // this is to remove negative elements
+            while(deque.size()>0 && pre[i] <= pre[deque.getLast()]){
+                deque.removeLast();
+            }
+
+            deque.push(i);
+        }
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+
+
+
 
 
 
