3d6b207a30824a6429a7a4edcb15d5d9df9e4b61
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-01-08 21:06:10 +0530

    HashMap Implementation base code

---
+++ Batch2/HashMap and Heap/HashMap/Implementation/Main.java
@@ -0,0 +1,57 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+class Main {
+
+    static class HashMap<K,V> {
+
+        class HashMapNode {
+            K key;
+            V value;
+
+            public HashMapNode(K key, V Value){
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private LinkedList<HashMapNode>[] buckets;
+        private int size;
+        private int number_of_buckets = 4;
+
+        public HashMap(){
+            size = 0;
+            initializeBuckets();
+        }
+
+        private void initializeBuckets(){
+            buckets = new LinkedList[number_of_buckets];
+
+            for(int i=0; i<buckets.length; i++){
+                buckets[i] = new LinkedList<>();
+            }
+        }
+
+        private int findBucketIndex(K key){
+            int hc = key.hashCode(); // -16 || 5;
+            int bucketIndex = Math.abs(hc) % number_of_buckets;
+
+            return bucketIndex;
+        }
+
+        public void put(K key, V value){}
+
+        public V get(K key){}
+
+        public boolean containsKey(K key){}
+
+        public V remove(K key){}
+
+        public ArrayList<K> keySet(){}
+    }
+    
+    public static void main(String[] args){
+        HashMap<String,Integer> map = new HashMap<>();
+
+        map.put("India",100);
+    }
+}
