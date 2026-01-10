2da6bf795f8b2d866368a2fa4162ce9dbe41607f
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-01-10 22:07:00 +0530

    Heap Implementation

--- Batch2/HashMap and Heap/Heap/Implementation/GenericPriorityQueue.java
+++ Batch2/HashMap and Heap/Heap/Implementation/GenericPriorityQueue.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
-
-class Main {
+class GenericPriorityQueue {
     static class PriorityQueue {
         ArrayList<Integer> data;
+        boolean isMinHeap;
 
-        public PriorityQueue(){
+        public PriorityQueue(boolean isMinHeap){
             data = new ArrayList<>();
+            this.isMinHeap = isMinHeap;
         }
 
         private void swap(int i, int j){
@@ -16,10 +17,18 @@ class Main {
             data.set(j,dataAtI);
         }
 
+        private boolean isMorePrior(int i, int j){
+            if(isMinHeap){ // if its a min heap, smaller element should be on the top
+                return data.get(i) < data.get(j);
+            } else {    // if its a max heap, larger element should be on the top
+                return data.get(i) > data.get(j);
+            }
+        }
+
         private void upHeapify(int childIdx){
             int parentIdx = (childIdx - 1)/2;
 
-            if(data.get(parentIdx) > data.get(childIdx)){
+            if(isMorePrior(childIdx, parentIdx)){
                 swap(parentIdx, childIdx);
                 upHeapify(parentIdx);
             }
@@ -36,11 +45,11 @@ class Main {
 
             int minIdx = parentIdx;
 
-            if(leftChildIdx < data.size() && data.get(leftChildIdx) < data.get(minIdx)){
+            if(leftChildIdx < data.size() && isMorePrior(leftChildIdx, minIdx)){
                 minIdx = leftChildIdx;
             }
 
-            if(rightChildIdx < data.size() && data.get(rightChildIdx) < data.get(minIdx)){
+            if(rightChildIdx < data.size() && isMorePrior(rightChildIdx, minIdx)){
                 minIdx = rightChildIdx;
             }
 
@@ -79,15 +88,16 @@ class Main {
     }
 
     public static void main(String[] args){
-        PriorityQueue pq = new PriorityQueue();
+        PriorityQueue pq = new PriorityQueue(true); // not a minHeap
         pq.add(10);
         pq.add(5);
         pq.add(7);
         pq.add(15);
+        pq.add(25);
+        pq.add(7);
 
-        System.out.println(pq.remove());
-        System.out.println(pq.remove());
-        System.out.println(pq.remove());
-        System.out.println(pq.remove());
+        while(pq.size() > 0){
+            System.out.println(pq.remove());
+        }
     }
 }
