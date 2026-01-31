3a9f397da58e31b55d89f16270af1451a807a82a
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-01-31 21:22:49 +0530

    Binary Tree Construction base

--- Batch2/Trees/Binary Tree/Construction/Main.java
+++ Batch2/Trees/Binary Tree/Construction/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class TreeNode {
     int data;
 
@@ -9,15 +11,69 @@ class TreeNode {
     }
 }
 
+class Pair {
+    TreeNode node;
+    int state;
+
+    public Pair(TreeNode node, int state){
+        this.node = node;
+        this.state = state;
+    }
+}
+
 class Main {
     public static TreeNode buildTree(Integer[] arr){
+        Stack<Pair> st = new Stack<>();
 
+        TreeNode root = null;
+
+        for(int i=0; i<arr.length; i++){
+            Integer ele = arr[i];
+
+            if(ele == null){
+                if(st.size() == 0){ // no tree possible, first node is null itself 
+                    return null;
+                } else if(st.peek().state == 0){
+                    st.peek().state++;
+                } else {
+                    st.pop();
+                }
+            } else {
+                TreeNode newNode = new TreeNode(ele);
+
+                if(st.size() == 0){
+                    root = newNode;
+                } else if(st.peek().state == 0){
+                    st.peek().node.left = newNode;
+
+                    st.peek().state++;
+                } else {
+                    st.peek().node.right = newNode;
+
+                    st.pop();
+                }
+
+                st.push(new Pair(newNode, 0));
+            }
+        }
+
+        return root;
     }
 
-    public static void main(Stringp[] args){
-        Integer[] arr = {10,20,40,null,60,null,50,70,null,null,null,30,80,90,null,null,100,null,null,null};
+    public static void display(TreeNode root){
+        if(root == null) return;
+
+        String nodeStructure = (root.left == null ? "." : root.left.data) + " <- " + root.data + " -> " + (root.right == null ? "." :  root.right.data);
+
+        System.out.println(nodeStructure);
+        display(root.left);
+        display(root.right);
+    }
+
+    public static void main(String[] args){
+        Integer[] arr = {10,20,40,null,60,null,null,50,70,null,null,null,30,80,90,null,null,100,null,null,null};
 
         TreeNode root = buildTree(arr);
-        
+        display(root);
     }
 }
