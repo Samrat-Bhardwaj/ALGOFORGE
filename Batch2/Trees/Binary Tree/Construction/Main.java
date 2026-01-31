2e985e6ac22abb071f0aca5882b59d401912b8a4
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-01-31 20:41:14 +0530

    Binary Tree Construction base

---
+++ Batch2/Trees/Binary Tree/Construction/Main.java
@@ -0,0 +1,23 @@
+class TreeNode {
+    int data;
+
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data){
+        this.data = data;
+    }
+}
+
+class Main {
+    public static TreeNode buildTree(Integer[] arr){
+
+    }
+
+    public static void main(Stringp[] args){
+        Integer[] arr = {10,20,40,null,60,null,50,70,null,null,null,30,80,90,null,null,100,null,null,null};
+
+        TreeNode root = buildTree(arr);
+        
+    }
+}
