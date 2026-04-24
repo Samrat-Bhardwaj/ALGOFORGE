0dcc77faed6c6b75373389bfb8506f9244af962d
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-04-24 20:43:55 +0530

    Three sum

--- Batch2/Arrays and Strings Revisited/Misc Problems/XSumProblems.java
+++ Batch2/Arrays and Strings Revisited/Misc Problems/XSumProblems.java
@@ -49,4 +49,65 @@ class XSumProblems {
 
         return ans;
     }
+
+    // Three sum (Leetcode 15) =============================
+    public void addFixedElement(List<List<Integer>> ans, List<List<Integer>> allPairs, int fixedEle){
+        for(List<Integer> pair: allPairs){
+            List<Integer> smallAns = new ArrayList<>();
+            smallAns.add(fixedEle);
+            smallAns.addAll(pair);
+
+            ans.add(smallAns);
+        }
+    }
+
+    public List<List<Integer>> getPairs(int[] nums, int si, int ei, int target){
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int i = si;
+        int j = ei;
+
+        while(i < j){
+            int sum = nums[i] + nums[j];
+
+            if(sum == target){
+                ans.add(Arrays.asList(nums[i],nums[j]));
+                i++;
+                j--;
+            } else if(sum < target){
+                i++;
+            } else {
+                j--;
+            }
+
+            while(i > si && i <= ei && nums[i-1] == nums[i]) i++;
+            while(j < ei && j >= si && nums[j+1] == nums[j]) j--;
+        }
+
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum_generic(int[] nums, int si, int ei, int target){
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=si; i<=ei; i++){
+            if(i > si && nums[i-1] == nums[i]) continue;
+
+            int fixedEle = nums[i];
+
+            int updatedTarget = target - fixedEle;
+
+            List<List<Integer>> allPairs = getPairs(nums,i+1,ei,updatedTarget);
+
+            if(allPairs.size() > 0){
+                addFixedElement(ans, allPairs, fixedEle);
+            }
+        }
+        return ans;
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        return threeSum_generic(nums,0,nums.length-1,0);
+    }
 }
