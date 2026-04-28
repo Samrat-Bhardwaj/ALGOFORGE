5dca13bc07cbeeb94e99908bbe9c574fe020dd91
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-04-28 23:21:36 +0530

    More Binary Search

--- Batch2/Arrays and Strings Revisited/Misc Problems/Questions.java
+++ Batch2/Arrays and Strings Revisited/Misc Problems/Questions.java
@@ -161,4 +161,171 @@ class Questions {
 
         return res;
     }
+
+    // Leetcode 134 (Gas station)
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total_gas = 0;
+        int total_cost = 0;
+
+        int current_gas = 0;
+        int start = 0;
+
+        for(int i=0; i<gas.length; i++){
+            current_gas += (gas[i] - cost[i]);
+
+            if(current_gas < 0){
+                start = i + 1;
+                current_gas = 0;
+            }
+
+            total_gas += gas[i];
+            total_cost += cost[i];
+        }
+
+        return (total_gas - total_cost) < 0 ? -1 : start;
+    }
+
+    // Leetcode 1539 =======================
+    // O(N);
+    public int findKthPositive(int[] arr, int k) {
+        int idx = -1;
+
+        for(int i=0; i<arr.length; i++){
+            int expected_num = (i+1);
+
+            int diff = arr[i] - expected_num;
+
+            if(diff < k){ 
+                idx = i; // the index till which our missing number is not present
+            } else {
+                break;
+            }
+        }
+
+        return idx + 1 + k;
+    }
+
+    public int findKthPositive(int[] arr, int k) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            int expected_num = (mid+1);
+            int diff = arr[mid] - expected_num;
+
+            if(diff < k){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right + 1 + k;
+    }
+
+    // Leetcode 33 (Search in rotated sorted Array)
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[left] <= nums[mid]){ // left to mid is sorted
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // mid to right is sorted
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    // Leetcode 81 ==================================
+    // Average case = log(N), worst case = O(N)
+    public boolean search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+            int mid = (left + right)/2;
+
+            if(nums[mid] == target || nums[right] == target){ // we may remove right from search range 
+                return true;
+            } else if(nums[left] < nums[mid]){ // left to mid is sorted
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if(nums[mid] < nums[right]) { // mid to right is sorted
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                right--; // or left++;
+            }
+        }
+
+        return false;
+    }
+
+    // Leetcode 4. Median of Two Sorted Arrays
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        if(A.length > B.length){ // swap
+            int[] temp = A;
+            A = B;
+            B = temp;
+        }
+
+        int n = A.length;
+        int m = B.length;
+
+        int total_ele = n + m;
+
+        int left = 0;
+        int right = n;
+
+        while(left <= right){
+            int mid = (left+right)/2; // left numbers of "A" arrays is from 0 to mid
+
+            int secondArrayEle = ((total_ele+1)/2) - (mid); // +1 makes sure left will have one more element
+
+            int aleft = (mid == 0) ? Integer.MIN_VALUE : A[mid-1] ;
+            int aright = (mid == n) ? Integer.MAX_VALUE: A[mid];
+            int bleft = (secondArrayEle == 0) ? Integer.MIN_VALUE : B[secondArrayEle-1];
+            int bright = (secondArrayEle == m) ? Integer.MAX_VALUE : B[secondArrayEle];
+
+            if(aleft <= bright && bleft <= aright){
+                // find mid according to even or odd elements
+                if(total_ele % 2 == 0){
+                    return ((Math.max(aleft, bleft) + Math.min(aright, bright))*1.0)/2.0;
+                } else {
+                    return Math.max(aleft, bleft) * 1.0;
+                }
+            } else if(bleft > aright){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return 0.0;   
+    }
+
+    
 }
