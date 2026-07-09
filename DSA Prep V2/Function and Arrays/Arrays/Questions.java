69776db48cf46e3980ff2fe1e3da4fa771d4b97e
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-07-09 22:06:54 +0530

    Array Questions

--- DSA Prep V2/Function and Arrays/Arrays/Questions.java
+++ DSA Prep V2/Function and Arrays/Arrays/Questions.java
@@ -1,10 +1,5 @@
 import java.util.Scanner;
 class Questions {
-    // span of array
-    public static int findSpan(int[] arr){
-
-    }
-
     public static int countElementsGreaterThanX(int[] arr, int x){
         int count = 0;
 
@@ -18,6 +13,83 @@ class Questions {
 
         return count;
     }
+    
+    // span of array
+    public static int findSpan(int[] arr){
+        if(arr.length == 0){ // edge case
+            return -1;
+        }
+
+        int max = Integer.MIN_VALUE; //-10^9 OR int max = arr[0];
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length; i++){
+            int ele = arr[i];
+
+            if(ele > max){
+                max = ele;
+            }
+            if(ele < min){
+                min = ele;
+            }
+        }
+
+        int span = max - min;
+        return span;
+    }
+
+    // Search in array (https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1)
+    public int search(int arr[], int tar) {
+        int ansIdx = -1;
+
+        for(int i=0; i<arr.length; i++){
+            int ele = arr[i];
+
+            if(ele == tar){
+                ansIdx = i;
+                break;
+            }
+        }
+
+        return ansIdx;
+    }
+
+    // Search in array (https://www.geeksforgeeks.org/problems/search-an-element-in-an-array-1587115621/1)
+    public int search(int arr[], int tar) {
+
+        for(int i=0; i<arr.length; i++){
+            int ele = arr[i];
+
+            if(ele == tar){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
