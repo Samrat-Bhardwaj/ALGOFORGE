821d922409d4540a551b54fb66f8454728b8ada7
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-09-10 20:48:29 +0530

    LinkedList Questions

--- DSA Prep V2/LinkedList/Questions/Main.java
+++ DSA Prep V2/LinkedList/Questions/Main.java
@@ -42,6 +42,53 @@ class Main {
         return prev; // new head
     }
 
+    // Leetcode 234 (Pallindrome LinkedList)
+    public boolean isPalindrome(ListNode head) {
+        // divide into two halves
+        ListNode midNode = findMiddleNode(head);
+
+        ListNode secondHalfHead = midNode.next;
+        midNode.next = null;
+
+        // reverse second half
+        secondHalfHead = revereseLinkedList(secondHalfHead);
+
+        // compare two halves
+        ListNode temp1 = head;  
+        ListNode temp2 = secondHalfHead;
+
+        while(temp1 != null && temp2 != null){
+            if(temp1.data != temp2.data){
+                return false;
+            }
+
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public static void main(String[] args){
         //
     }
--- DSA Prep V2/LinkedList/Questions/questions.cpp
+++ DSA Prep V2/LinkedList/Questions/questions.cpp
@@ -13,6 +13,63 @@ ListNode* middleNode(ListNode* head) {
     return slow;
 }
 
+// helper function to find middle of linkedList (first middle in case of even)
+ListNode* findMiddleNode(ListNode* head){
+    ListNode* slow = head;
+    ListNode* fast = head;
+    
+    while(fast->next && fast->next->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+// helper function to reverse linkedlist and return new head
+ListNode* revereseLinkedList(ListNode* head){
+    ListNode* curr = head;
+    ListNode* prev = nullptr;
+
+    while(curr){
+        ListNode* currKaNext = curr->next;
+
+        curr->next = prev;
+
+        prev = curr;
+        curr = currKaNext;
+    }
+
+    return prev; // new head
+}
+
+// Leetcode 234 (Pallindrome LinkedList)
+bool isPalindrome(ListNode* head) {
+    // divide into two halves
+    ListNode* midNode = findMiddleNode(head);
+
+    ListNode* secondHalfHead = midNode->next;
+    midNode->next = NULL;
+
+    // reverse second half
+    secondHalfHead = revereseLinkedList(secondHalfHead);
+
+    // compare two halves
+    ListNode* temp1 = head;  
+    ListNode* temp2 = secondHalfHead;
+
+    while(temp1 && temp2){
+        if(temp1->val != temp2->val){
+            return false;
+        }
+
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+
+    return true;
+}
+
 void main(){
     
 }
