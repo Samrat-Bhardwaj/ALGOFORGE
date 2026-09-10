dd10af86a5fae9722772a0b907ea2e27250a6bcf
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-09-10 20:27:48 +0530

    LinkedList Questions

--- DSA Prep V2/LinkedList/Questions/Main.java
+++ DSA Prep V2/LinkedList/Questions/Main.java
@@ -12,6 +12,36 @@ class Main {
         return slow;
     }
 
+    // helper function to find middle of linkedList (first middle in case of even)
+    public ListNode findMiddleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+    
+    // helper function to reverse linkedlist and return new head
+    public ListNode revereseLinkedList(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(curr != null){
+            ListNode currKaNext = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+            curr = currKaNext;
+        }
+
+        return prev; // new head
+    }
+
     public static void main(String[] args){
         //
     }
