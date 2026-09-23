41ec81291fafbc2a0bde22a6c97c91854872aafa
Samrat Bhardwaj <bsamrat47@gmail.com>
2026-09-23 20:05:09 +0530

    LinkedList QUestions

--- DSA Prep V2/LinkedList/Questions/Main.java
+++ DSA Prep V2/LinkedList/Questions/Main.java
@@ -290,6 +290,162 @@ class Main {
             return removeCopy(head);
         }
     }
+
+    // Leetcode 83 Delete duplicates keeping the first one ================================
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while(curr != null && curr.next != null){
+            if(curr.val == curr.next.val){ // in case of duplicate, remove the next node by changing curr.next
+                curr.next = curr.next.next;
+            } else { // in case no duplicate, move to next 
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+
+    // leetcode 82 Delete all the duplicates ==============================================
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode curr = dummy;
+
+        while(curr.next != null && curr.next.next != null){
+            if(curr.next.val == curr.next.next.val){
+                ListNode temp = curr.next;
+                int duplicateValue = curr.next.val;
+
+                while(temp!=null && temp.val == duplicateValue){
+                    temp = temp.next;
+                }
+
+                curr.next = temp;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    // Leetcode 237 (How to virtually delete a node) =====================================
+    public void deleteNode(ListNode node) {
+        ListNode nodeKaNext = node.next;
+
+        // delete the next node
+        node.next = node.next.next;
+        
+        // copy nodeKaNext data here
+        node.val = nodeKaNext.val;
+    }
+
+    // Leetcode 114 (LinkedList cycle)======================================
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Find the starting point of LinkedList cycle (Leetcode 142)
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                break;
+            }
+        }
+
+        if(slow != fast){ // no cycle exist
+            return null;
+        }
+
+        // Find starting point
+        fast = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow; // or return fast;
+    }
+
+    // Get intersection point of Y Linkedlist (Leetcode 160) ===================
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode temp = headA;
+
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        ListNode tailA = temp;
+
+        tailA.next = headA; // or headB
+        ListNode intersectionNode = detectCycle(headB); // if tail is connected to headB, send headA here
+
+        tailA.next = null; // fixing the list again
+
+        return intersectionNode;
+    }
+
+    // Leetcode 21 (Merge two sorted lists) ================================
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while(temp1 != null && temp2 != null){
+            if(temp1.val < temp2.val){
+                curr.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                curr.next = temp2;
+                temp2 = temp2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        // connect rest of the lists
+        if(temp2 == null){
+            curr.next = temp1;
+        } else {
+            curr.next = temp2;
+        }
+
+        return dummy.next;
+    }
     
     // Create browser History (Leetcode 1472) ======================================
     class BrowserHistory {
@@ -375,54 +531,54 @@ class Main {
 
     // Leetcode 92 ======================
     class Solution {
-    public ListNode getNodeAt(ListNode head, int idx){
-        ListNode temp = head;
+        public ListNode getNodeAt(ListNode head, int idx){
+            ListNode temp = head;
 
-        for(int i=0; i<idx; i++){
-            temp = temp.next;
+            for(int i=0; i<idx; i++){
+                temp = temp.next;
+            }
+
+            return temp;
+        }
+        public ListNode revereseLinkedList(ListNode head){
+            ListNode curr = head;
+            ListNode prev = null;
+
+            while(curr != null){
+                ListNode currKaNext = curr.next;
+
+                curr.next = prev;
+
+                prev = curr;
+                curr = currKaNext;
+            }
+
+            return prev; // new head
         }
 
-        return temp;
-    }
-    public ListNode revereseLinkedList(ListNode head){
-        ListNode curr = head;
-        ListNode prev = null;
+        public ListNode reverseBetween(ListNode head, int left, int right) {
+            ListNode newHead = new ListNode(-1);
+            newHead.next = head;
 
-        while(curr != null){
-            ListNode currKaNext = curr.next;
+            ListNode prevNode = getNodeAt(newHead, left-1);
+            ListNode endOfList2 = getNodeAt(newHead, right);
+            ListNode nextNode = endOfList2.next;
 
-            curr.next = prev;
+            ListNode head2 = prevNode.next;
 
-            prev = curr;
-            curr = currKaNext;
+            // get the list out
+            prevNode.next = null;
+            endOfList2.next = null;
+
+            ListNode reversedHead = revereseLinkedList(head2);
+
+            prevNode.next = reversedHead;
+            if(head2 != null)
+            head2.next = nextNode;
+
+            return newHead.next;
         }
-
-        return prev; // new head
     }
-
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode newHead = new ListNode(-1);
-        newHead.next = head;
-
-        ListNode prevNode = getNodeAt(newHead, left-1);
-        ListNode endOfList2 = getNodeAt(newHead, right);
-        ListNode nextNode = endOfList2.next;
-
-        ListNode head2 = prevNode.next;
-
-        // get the list out
-        prevNode.next = null;
-        endOfList2.next = null;
-
-        ListNode reversedHead = revereseLinkedList(head2);
-
-        prevNode.next = reversedHead;
-        if(head2 != null)
-        head2.next = nextNode;
-
-        return newHead.next;
-    }
-}
 
 
     
